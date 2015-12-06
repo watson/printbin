02629bd424ba1d451054094f60000191d3adf358
Thomas Watson Steen <w@tson.dk>
2015-12-05 21:10:28 -0500

    Output stuff

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 'use strict'
 
+console.log('Now advertising the printbin printer on your local network.')
+
 require('zeroconf')().tcp.publish({ type: 'ipp', host: 'ipp.printb.in', port: 80, name: 'printbin' })
