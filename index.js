0a1609844d97dc6c70194906a75627ca0fb60384
Thomas Watson Steen <w@tson.dk>
2015-12-05 21:07:29 -0500

    Print to ipp.printb.in

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 #!/usr/local/env node
 'use strict'
 
-require('zeroconf')().tcp.publish({ type: 'ipp', host: 'printbin-ipp.herokuapp.com', port: 80, name: 'printbin' })
+require('zeroconf')().tcp.publish({ type: 'ipp', host: 'ipp.printb.in', port: 80, name: 'printbin' })
