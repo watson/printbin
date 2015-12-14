f023000c33b4c9fa3cb55d321025624a72c61dc7
Thomas Watson Steen <w@tson.dk>
2015-12-14 23:22:46 +0100

    Fix bonjour require statement

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
 
 console.log('Now advertising the printbin printer on your local network.')
 
-require('zeroconf')().tcp.publish({ type: 'ipp', host: 'ipp.printb.in', port: 80, name: 'printbin' })
+require('bonjour')().tcp.publish({ type: 'ipp', host: 'ipp.printb.in', port: 80, name: 'printbin' })
